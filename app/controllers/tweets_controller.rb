4cf57119bfd3368f65cdbf85ad074af885478998
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-02 18:11:25 +0900

    コントローラーの共通部分リファクタリング

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: %i[edit update destroy]
+
   def toppage
   end
 
@@ -20,11 +22,9 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
-    @tweet = Tweet.find(params[:id])
     if @tweet.update(tweet_params)
       redirect_to tweets_path
     else
@@ -33,7 +33,6 @@ class TweetsController < ApplicationController
   end
 
   def destroy
-    @tweet = Tweet.find(params[:id])
     @tweet.destroy
     redirect_to tweets_path
   end
@@ -42,6 +41,10 @@ class TweetsController < ApplicationController
 
   def tweet_params
     params.require(:tweet).permit(:content)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 
 end
