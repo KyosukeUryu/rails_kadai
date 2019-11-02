ac52235406447f9fa3bb1a2f10f04ad8afc407f1
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-02 16:44:37 +0900

    コントローラー初期書き込み

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,28 @@
 class TweetsController < ApplicationController
   def toppage
   end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
 end
