0048f50892d0ad1e54fa122e0dde35807cdcc3b1
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-03 13:34:33 +0900

    コンフリクト修正

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,7 @@ Bundler.require(*Rails.groups)
 module RailsKadai
   class Application < Rails::Application
     config.load_defaults 5.2
-<<<<<<< HEAD
-
-=======
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
->>>>>>> develop
   end
 end
